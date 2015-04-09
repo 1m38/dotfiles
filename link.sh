2187ac81cf5be84a403e6ed275b616a94a5b7a2a
Masaya Uno <uno@nlp.ist.i.kyoto-u.ac.jp>
2015-04-09 22:39:20 +0900

    add sshconfig

--- link.sh
+++ link.sh
@@ -4,3 +4,4 @@ ln -sf ~/dotfiles/.emacs ~/.emacs
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.screenrc ~/.screenrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/sshconfig ~/.ssh/config
