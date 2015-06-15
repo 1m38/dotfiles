518337467960fdd1e25a3f1896f8134dbed466e5
Masaya Uno <uno@nlp.ist.i.kyoto-u.ac.jp>
2015-06-15 15:59:03 +0900

    modified:   ChangeLog modified:   emacs_inits/30_helm.el   選択行のハイライト色を変更(magenta)

--- emacs_inits/30_helm.el
+++ emacs_inits/30_helm.el
@@ -33,3 +33,7 @@
 		      ;; and not required because the directory name is prepended
 		      (substring input-pattern 1)
 		    (concat ".*" input-pattern))))))
+
+(custom-set-faces
+ '(helm-selection ((t (:background "magenta" :distant-foreground "black"))))
+ )
