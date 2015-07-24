19eb7d925dcca586b18cca95f932aac6a3acbd44
Masaya Uno <uno@nlp.ist.i.kyoto-u.ac.jp>
2015-07-24 13:52:24 +0900

    modified:   emacs_inits/26_magit.el   faceに関する設定変更
    modified:   ChangeLog

--- emacs_inits/26_magit.el
+++ emacs_inits/26_magit.el
@@ -1,5 +1,15 @@
 (require 'magit)
 (setq magit-last-seen-setup-instructions "1.4.0")
 (setq magit-auto-revert-mode nil)
-(set-face-foreground 'magit-diff-add "white")
-(set-face-background 'magit-item-highlight "green")
+(eval-after-load 'magit
+  '(progn
+     (set-face-foreground 'magit-diff-added-highlight "white")
+     (set-face-background 'magit-diff-added-highlight "green")
+     (set-face-foreground 'magit-diff-removed-highlight "red")
+     (set-face-background 'magit-diff-removed-highlight "green")
+
+     (set-face-foreground 'magit-diff-added "white")
+     (set-face-background 'magit-diff-added "blue")
+     (set-face-foreground 'magit-diff-removed "black")
+     (set-face-background 'magit-diff-removed "blue")
+     ))
