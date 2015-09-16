ea291847b8fa7262fe939f15cb0fe9df946a8727
Masaya Uno <uno@nlp.ist.i.kyoto-u.ac.jp>
2015-09-16 13:54:25 +0900

    link.sh: .emacs.d ディレクトリを作るように, .zlogoutを削除
    	modified:   ChangeLog
    	modified:   link.sh

--- link.sh
+++ link.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-#ln -sf ~/dotfiles/.emacs ~/.emacs
+if [ ! -d ~/.emacs.d ]; then
+    mkdir -p ~/.emacs.d
+fi
 ln -sf ~/dotfiles/emacs_init.el ~/.emacs.d/init.el
 ln -sf ~/dotfiles/emacs_inits ~/.emacs.d/inits
 
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.screenrc ~/.screenrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.zlogout ~/.zlogout
+# ln -sf ~/dotfiles/.zlogout ~/.zlogout
 ln -sf ~/dotfiles/sshconfig ~/.ssh/config
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
