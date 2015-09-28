d91d440d185f31a81d0b0906b3ce39c3611045de
Masaya Uno <uno@nlp.ist.i.kyoto-u.ac.jp>
2015-09-28 15:33:41 +0900

    emacs:theme設定(tango-dark)
    	modified:   ChangeLog
    	modified:   emacs_init.el
    	modified:   emacs_inits/26_magit.el
    	new file:   emacs_inits/98_theme.el

--- emacs_init.el
+++ emacs_init.el
@@ -8,3 +8,20 @@
   (require 'init-loader)
   (init-loader-load "~/.emacs.d/inits")
   )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ ;; '(custom-safe-themes
+ ;;   (quote
+ ;;    ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default)))
+ '(markdown-css-paths (quote ("markdown.css")))
+ '(safe-local-variable-values (quote ((encoding . utf-8)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(helm-selection ((t (:background "magenta" :distant-foreground "black"))))
+ '(hl-line ((t (:background "black")))))
--- emacs_inits/26_magit.el
+++ emacs_inits/26_magit.el
@@ -1,36 +1,3 @@
 (require 'magit)
 (setq magit-last-seen-setup-instructions "1.4.0")
 (setq magit-auto-revert-mode nil)
-(eval-after-load 'magit
-  '(progn
-     ;; (set-face-background 'magit-item-highlight nil)
-
-     ;; (set-face-attribute 'magit-diff-hunk-heading nil
-     ;; 			 :foreground "blue"
-     ;; 			 :background nil)
-
-     ;; (set-face-attribute 'magit-diff-hunk-heading-highlight nil
-     ;; 			 :foreground "blue"
-     ;; 			 :background "white")
-
-     ;; (set-face-attribute 'magit-diff-context-highlight nil
-     ;; 			 :foreground "black"
-     ;; 			 :background "white")
-
-     ;; (set-face-attribute 'magit-diff-added-highlight nil
-     ;; 			 :foreground "white"
-     ;; 			 :background "green")
-     ;; (set-face-attribute 'magit-diff-removed-highlight nil
-     ;; 			 :foreground "red"
-     ;; 			 :background "green")
-
-     ;; (set-face-attribute 'magit-diff-added nil
-     ;; 			 :foreground "green"
-     ;; 			 :background nil)
-     ;; (set-face-attribute 'magit-diff-removed nil
-     ;; 			 :foreground "blue"
-     ;; 			 :background nil)
-
-     (set-face-attribute 'magit-diff-add nil
-			 :foreground "yellow")
-     ))
