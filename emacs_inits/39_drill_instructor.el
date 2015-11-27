e534d1c3f8e977eeb61d2740b189da39cc3a8a51
Masaya Uno <uno@nlp.ist.i.kyoto-u.ac.jp>
2015-11-27 15:45:03 +0900

    emacs_inits/39_drill_instructor.el: 無効化
    	modified:   ChangeLog
    	modified:   emacs_inits/39_drill_instructor.el

--- emacs_inits/39_drill_instructor.el
+++ emacs_inits/39_drill_instructor.el
@@ -1,6 +1,6 @@
 ;;; drill instructor(鬼軍曹.el)
 ;;; Installation: M-x auto-install-from-emacswiki drill-instructor.el
-(if (require 'drill-instructor nil t)
-    (setq drill-instructor-global t)
-  )
+;; (if (require 'drill-instructor nil t)
+;;     (setq drill-instructor-global t)
+;;   )
 
