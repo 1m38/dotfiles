d40803d2196570652ef9720e4b7f639f99f62f5a
Masaya Uno <uno@nlp.ist.i.kyoto-u.ac.jp>
2016-03-25 17:32:43 +0900

    emacs_inits/56_python.el: autopep8を削除(elpyを使用)
    	modified:   ChangeLog
    	modified:   emacs_inits/56_python.el

--- emacs_inits/56_python.el
+++ emacs_inits/56_python.el
@@ -6,16 +6,3 @@
 ;;; elpy
 (elpy-enable)
 (elpy-use-ipython)
-;; autopep8
-;; http://qiita.com/fujimisakari/items/74e32eddb78dff4be585
-;; source: github:fujimisakari/py-autopep8.el
-(el-get-bundle! py-autopep8
-    :type github :pkgname "fujimisakari/py-autopep8.el")
-(add-hook 'python-mode-hook
-	  (lambda ()
-	    (define-key python-mode-map (kbd "C-c F") 'py-autopep8)          ; バッファ全体のコード整形
-	    (define-key python-mode-map (kbd "C-c f") 'py-autopep8-region)   ; 選択リジョン内のコード整形
-	    ))
-
-;; 保存時にバッファ全体を自動整形する
-;; (add-hook 'before-save-hook 'py-autopep8-before-save)
