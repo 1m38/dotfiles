e373bc4056f4a09d86c013575924ba28fdf2a0df
Masaya Uno <uno@nlp.ist.i.kyoto-u.ac.jp>
2016-04-22 14:52:19 +0900

    init.sh -> init.zsh: OSごとに処理を分ける
    	modified:   ChangeLog
    	renamed:    init.sh -> init.zsh

--- init.zsh
+++ init.zsh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/zsh
 
-if [ ! -d ~/.emacs.d ]; then
+if [[ ! -d ~/.emacs.d ]]; then
     mkdir -p ~/.emacs.d
 fi
 ln -sf ~/dotfiles/emacs_init.el ~/.emacs.d/init.el
@@ -10,16 +10,23 @@ ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.screenrc ~/.screenrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 # hostごとの設定
-if [ `hostname -d` = "lang-private.kuee.kyoto-u.ac.jp" ]; then
-    # sshconfig: 研究室内ではlocal鍵を使う
-    ln -sf ~/dotfiles/sshconfig_k2l ~/.ssh/config
-    # gxprc
-    ln -sf ~/dotfiles/gxprc ~/gxprc
-else
-    ln -sf ~/dotfiles/sshconfig ~/.ssh/config
-fi
+case $OSTYPE in
+    linux*)
+	if [[ `hostname -d` = "lang-private.kuee.kyoto-u.ac.jp" ]]; then
+	    # sshconfig: 研究室内ではlocal鍵を使う
+	    ln -sf ~/dotfiles/sshconfig_k2l ~/.ssh/config
+	    # gxprc
+	    ln -sf ~/dotfiles/gxprc ~/gxprc
+	else
+	    ln -sf ~/dotfiles/sshconfig ~/.ssh/config
+	fi
+	;;
+    *)
+	ln -sf ~/dotfiles/sshconfig ~/.ssh/config
+	;;
+esac
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-if [ ! -d ~/.vimbackup ]; then
+if [[ ! -d ~/.vimbackup ]]; then
     mkdir -p ~/.vimbackup
 fi
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
