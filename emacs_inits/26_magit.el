31ea66ca118f499476749c3a9619b972c4a7a475
Masaya Uno <uno@nlp.ist.i.kyoto-u.ac.jp>
2017-07-09 19:26:28 +0900

    emacs_inits/26_magit.el: 日本語文字化け対策

--- emacs_inits/26_magit.el
+++ emacs_inits/26_magit.el
@@ -1,3 +1,10 @@
 (require 'magit)
 (setq magit-last-seen-setup-instructions "1.4.0")
 (setq magit-auto-revert-mode nil)
+
+;; fishでの文字化け対策
+;; http://qiita.com/alpha22jp/items/01e614474e7dbfd78305
+(add-to-list 'process-coding-system-alist '("git" utf-8 . utf-8))
+(add-hook 'git-commit-mode-hook
+          '(lambda ()
+             (set-buffer-file-coding-system 'utf-8)))
