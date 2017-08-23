8ad7ec3b4be217342b350f7c640df18391678b2b
Masaya Uno <uno@nlp.ist.i.kyoto-u.ac.jp>
2017-08-23 16:12:43 +0900

    utils/colors.py: terminal colorsを一覧表示

---
+++ utils/colors.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""print terminal 256 colors"""
+
+from __future__ import print_function
+
+
+def main():
+    for i in range(256):
+        print("\033[48;05;{cc}m {cc:3d} \033[0m\033[38;05;{cc}m {cc:3d} \033[0m".format(cc=i),
+              end="")
+        if (i + 1) % 8 == 0:
+            print()
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
