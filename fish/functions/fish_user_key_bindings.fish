a717188418bea3e88f1dc2772437882a10382b5f
Masaya Uno <uno@nlp.ist.i.kyoto-u.ac.jp>
2019-04-07 14:52:29 +0900

    fish環境をリセット

--- fish/functions/fish_user_key_bindings.fish
+++
@@ -1,3 +0,0 @@
-function fish_user_key_bindings
-	bind \cd delete-char
-end
--- fish/functions/h.fish
+++
@@ -1,3 +0,0 @@
-function h --description 'show history with time'
-    builtin history --show-time='%Y/%m/%d %H:%M:%S  '
-end
